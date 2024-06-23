f6b5bd5979fccd0c3bc65de3d9462eb124494d31
syntaxbullet <syntaxbullet@protonmail.com>
2024-06-23 20:52:02 +0200

    chore: change lambas to regular functions, changed unused map to foreach instead.

--- src/KimikoPluginManager.ts
+++ src/KimikoPluginManager.ts
@@ -31,18 +31,25 @@ const pluginRegistry: PluginRegistry = new Map<string, PluginExports>();
 async function getPlugins() {
   const plugins = new Map<string, PluginFileMap>();
 
-  const isDir = (dir: string) => fs.lstatSync(dir).isDirectory();
-  const hasRC = (pluginPath: string) =>
-    fs.existsSync(path.resolve(pluginPath, 'kimiko.pluginrc.json'));
-  const isPlugin = (pluginPath: string) => isDir(pluginPath) && hasRC(pluginPath);
+  function isDir(dir: string) {
+    return fs.lstatSync(dir).isDirectory();
+  }
+
+  function hasRC(pluginPath: string) {
+    return fs.existsSync(path.resolve(pluginPath, 'kimiko.pluginrc.json'));
+  }
+
+  function isPlugin(pluginPath: string) {
+    return isDir(pluginPath) && hasRC(pluginPath);
+  }
 
   const pluginDirs = fs.readdirSync(PLUGINS_DIR);
   const pluginPaths = pluginDirs.map((dir) => path.resolve(PLUGINS_DIR, dir));
   const pluginPathsFiltered = pluginPaths.filter(isPlugin);
 
-  const getJSON = (pluginPath: string, fileName: string) => {
+  function getJSON(pluginPath: string, fileName: string) {
     return JSON.parse(fs.readFileSync(path.resolve(pluginPath, fileName), 'utf-8'));
-  };
+  }
 
   pluginPathsFiltered.map((pluginPath) => {
     const packageJSON = getJSON(pluginPath, 'package.json');
@@ -62,11 +69,16 @@ async function getPlugins() {
 function checkDependencies(plugins: Map<string, PluginFileMap>) {
   logger.debug('Checking plugin dependencies...');
   const list = Array.from(plugins.keys());
-  const getDependencies = (plugin: string) => plugins.get(plugin)!.kimikoPluginRC.dependencies;
 
-  const isMissingDependency = (plugin: string) => {
-    return getDependencies(plugin).some((dep: string) => !list.includes(dep));
-  };
+  function getDependencies(plugin: string) {
+    return plugins.get(plugin)!.kimikoPluginRC.dependencies;
+  }
+
+  function isMissingDependency(plugin: string) {
+    return getDependencies(plugin).some(function (dep) {
+      return !list.includes(dep);
+    });
+  }
 
   const missingDependencies = list.filter((plugin) => isMissingDependency(plugin));
 
@@ -86,7 +98,11 @@ function topologicalSort(plugins: Map<string, PluginFileMap>) {
   const dependencies = list.map((plugin) => plugins.get(plugin)!.kimikoPluginRC.dependencies);
   const order: string[] = [];
   const visited: Set<string> = new Set();
-  const isCyclic = (plugin: string) => dependencies[list.indexOf(plugin)].includes(plugin);
+
+  function isCyclic(plugin: string) {
+    return dependencies[list.indexOf(plugin)].includes(plugin);
+  }
+
   const cyclicDependencies = list.filter(isCyclic);
 
   if (cyclicDependencies.length > 0) {
@@ -98,11 +114,11 @@ function topologicalSort(plugins: Map<string, PluginFileMap>) {
     if (visited.has(plugin)) return;
 
     visited.add(plugin);
-    dependencies[list.indexOf(plugin)].map(visit);
+    dependencies[list.indexOf(plugin)].forEach(visit);
     order.push(plugin);
   };
 
-  list.map(visit);
+  list.forEach(visit);
   logger.debug(`Plugins sorted: ${order.join(' -> ')} `);
   return { order, plugins };
 }
